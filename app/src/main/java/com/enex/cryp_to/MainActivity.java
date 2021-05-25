2f904a8078a86187c321ba82be87802bccbccd78
unknown <namelessxcoded@gmail.com>
2021-05-25 18:58:15 +0545

    Added fetch news feature

--- app/src/main/java/com/enex/cryp_to/MainActivity.java
+++ app/src/main/java/com/enex/cryp_to/MainActivity.java
@@ -1,51 +1,188 @@
 package com.enex.cryp_to;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout layout;
+    ListView newslist;
+    TextView newsholderview;
+    ArrayAdapter<String> adapter;
+    Toolbar toolbar;
     int dimen,margin;
+    RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-         // Splash screen control
-        setTheme(R.style.Theme_CrypTo);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // toolbar configuration
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Toolbar configuration ends here
         layout = findViewById(R.id.scrollerView);
         dimen = dpToPx(150,this);
         margin = dpToPx(20,this);
+
         holderImageSetup(layout,dimen,margin);
 
         // Contact developer control
-        findViewById(R.id.contactDeveloper).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = "_maneesh_pandey";
-                try {
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse("http://instagram.com/_u/" + username));
-                    intent.setPackage("com.instagram.android");
-                    startActivity(intent);
+
+        queue = Volley.newRequestQueue(this);
+        fetchNewsData();
+        newslist = findViewById(R.id.newsList);
+
+    }
+
+    public void enterNewsDataInList(ArrayList<String> dataList){
+        newsholderview = findViewById(R.id.newstextview);
+        newsholderview.setText("Crypto News");
+        adapter = new ArrayAdapter<String>(this,R.layout.list_view,R.id.listOfNews,dataList);
+        newslist.setAdapter(adapter);
+        newslist.getDivider().setColorFilter(Color.WHITE, PorterDuff.Mode.LIGHTEN);
+    }
+
+    public void fetchNewsData(){
+        News news = new News(this,queue,20);
+        new Thread(news).start();
+    }
+    public class News implements Runnable{
+        Context context;
+        RequestQueue queue;
+        int lengthOfNews;
+        public News(Context context, RequestQueue queue,int lengthOfNews) {
+            this.context = context;
+            this.queue = queue;
+            this.lengthOfNews = lengthOfNews;
+        }
+        String title;
+        ArrayList<String> dataList,sourcelist;
+        ArrayAdapter<String> arrayAdapter;
+        @Override
+        public void run() {
+            String[] source = new String[lengthOfNews];
+            String[] newsDataArray = new String[lengthOfNews];
+            String apiurl = "https://cryptopanic.com/api/v1/posts/?auth_token=bde4b75c734b5340f15130617bea538744d64dc7";
+            JsonObjectRequest jsonData = new JsonObjectRequest(Request.Method.GET, apiurl, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                JSONArray result = response.getJSONArray("results");
+                                if (result.length()>0){
+                                    for (int i=0;i<lengthOfNews;i++){
+                                        JSONObject data = result.getJSONObject(i);
+                                        title = "#"+(int)(i+1)+" "+ data.getString("title");
+                                        newsDataArray[i] = title;
+                                        source[i] = "Source: "+ data.getString("domain");
+                                    }
+                                    dataList = new ArrayList<>(Arrays.asList(newsDataArray));
+                                    enterNewsDataInList(dataList);
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Error", "Error: " + error);
+                    if (error instanceof NetworkError || error instanceof NoConnectionError || error instanceof TimeoutError) {
+                        toast("It seems like you do not have internet connection!");
+                    } else {
+                        toast("Error: " + error.getMessage());
+                    }
                 }
-                catch (android.content.ActivityNotFoundException anfe)
-                {
-                    toast("Instagram app not found! Opening instagram in browser.");
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("https://www.instagram.com/" + username)));
-                }
-            }
-        });
+            });
+            queue.add(jsonData);        }
+    }
+
+    public void gotoDeveloperIg(){
+        String username = "_maneesh_pandey";
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://instagram.com/_u/" + username));
+            intent.setPackage("com.instagram.android");
+            startActivity(intent);
+        }
+        catch (android.content.ActivityNotFoundException anfe)
+        {
+            toast("Instagram app not found! Opening instagram in browser.");
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.instagram.com/" + username)));
+        }
+    }
+
+    public void gotoAboutpage(){
+        toast("About us page is yet to be made!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.d("itemid","Id: "+item.getItemId());
+        /*
+            Item ids
+                - Setting: 2131296678
+                - Contact developer: 2131296369
+                -About app: 2131296270
+         */
+        switch (item.getItemId()){
+            case R.id.settings:
+                toast("Setting page is yet to be made!");
+                break;
+            case R.id.contactDeveloper:
+                gotoDeveloperIg();
+                break;
+            case R.id.aboutApp:
+                gotoAboutpage();
+                break;
+            default:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
     public static int dpToPx(int dp, Context context) {
         float density = context.getResources().getDisplayMetrics().density;
---
+++ readme.md
@@ -0,0 +1,15 @@
+# **About Cryp-To**
+
+**Cryp-To** - A name derived from **Cryptocurrency** is an android application developed by **Maneesh Pandey** aka **NamelessX Dev** or **En3X**. This application is build with the purpose of providing everyone with knowledge of what the rate of cryptocurrency is going on in market.
+
+**Cryp-To** does not promote trading any kind of currency and it's sole purpose is to **provide information**. Trading is all upon users themselves and developer is not responsible for any kind of losses users had to face while trading, using this app as an reference. 
+
+The application is still in beta and can provide information about 14 crypto currency right now. Any suggestions, feedbacks are welcome. You can always contact the developer via **[Instagram](https://www.instagram.com/_maneesh_pandey)**.
+
+> *Note: Cryp-To fetches data about crypto currency using API. The data may or may not be a hundred percent accurate. If you are planning on trading using this app as reference, make sure to cross check the rates.
+
+------
+
+
+
+> The app is developed by solo developer. I am not earning millions of dollars from the app and to be very honest, it is not even my target! So though every designing works is done by myself, there might be a incidence where it matches with someone else's design. In such scenario, please hit me up for its modification and clarity.
